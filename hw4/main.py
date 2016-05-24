b720185af850d0e62298dcba4b138684510bc10e
SungMinCho <tjdals4565@gmail.com>
2016-05-24 14:35:34 +0900

    Minor changes

--- hw4/main.py
+++ hw4/main.py
@@ -472,10 +472,11 @@ def main():
     glClearColor(0.0, 0.0, 0.0, 0.0)
     glShadeModel(GL_SMOOTH);
 
-    glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
-    glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
+    #glMaterialfv(GL_FRONT, GL_SPECULAR, mat_specular)
+    #glMaterialfv(GL_FRONT, GL_SHININESS, mat_shininess)
     
     glLightfv(GL_LIGHT0, GL_DIFFUSE, [1.0,1.0,1.0,1.0])
+    glLightfv(GL_LIGHT0, GL_SPECULAR, [1.0,1.0,1.0,1.0])
     glLightfv(GL_LIGHT0, GL_POSITION, light_position)
 
     glLightfv(GL_LIGHT1, GL_DIFFUSE, [1.0,0.0,0.0,1.0])
